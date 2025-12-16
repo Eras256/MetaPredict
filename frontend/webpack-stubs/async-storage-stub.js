93c1f5abb10ef71686d51324d176e85efd461ab5
Eras256 <ticketsafe4@gmail.com>
2025-12-16 16:12:38 -0600

    Add webpack stubs for async-storage and pino-pretty

---
+++ frontend/webpack-stubs/async-storage-stub.js
@@ -0,0 +1,13 @@
+// Stub para @react-native-async-storage/async-storage
+// Este módulo es opcional y solo se usa en React Native, no en web
+module.exports = {
+  getItem: async () => null,
+  setItem: async () => {},
+  removeItem: async () => {},
+  clear: async () => {},
+  getAllKeys: async () => [],
+  multiGet: async () => [],
+  multiSet: async () => {},
+  multiRemove: async () => {},
+};
+
---
+++ frontend/webpack-stubs/pino-pretty-stub.js
@@ -0,0 +1,4 @@
+// Stub para pino-pretty
+// Este módulo es opcional y solo se usa para desarrollo, no en producción
+module.exports = () => (stream) => stream;
+
